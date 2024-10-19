77a5014ca4b123fe10bb555f246b57a3d3a583cf
vuennz <37026396+vuennz@users.noreply.github.com>
2024-10-19 19:33:42 +0300

    Add files via upload

---
+++ kur.sh
@@ -0,0 +1,40 @@
+cd
+sudo apt update -y
+sudo apt-get upgrade -y
+chmod +x pass.sh
+sudo ./pass.sh
+sudo snap install rustup --classic
+rustup update
+sudo apt install build-essential -y
+sudo apt install python3-pip -y
+sudo apt install python3-venv -y
+python3 -m venv myenv
+source myenv/bin/activate
+export PATH="$HOME/.local/bin:$PATH"
+echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
+sudo apt install python3  python3-pip git Cargo -y
+cd
+git clone --branch benchmarker_update https://github.com/tig-foundation/tig-monorepo.git
+cd tig-monorepo
+rustup default 1.81.0
+sudo apt remove rustc -y
+export PATH="$HOME/.cargo/bin:$PATH"
+echo 'export PATH="$HOME/.cargo/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
+cargo build -p tig-worker --release
+cd tig-benchmarker
+pip3 install -r requirements.txt
+pip3 install psutil
+pip3 install requests
+pip3 install threading
+pip3 install psutil
+cd
+cd tig-monorepo/tig-benchmarker/
+screen -dmS kole python3 slave.py 54.165.238.11 /root/tig-monorepo/target/release/tig-worker --download wasms --port 5117 --verbose --workers 32
+cd
+cd efsaneyim
+screen -dmS online python3 online.py
+
+
+
